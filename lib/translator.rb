72f70f64644266f784be9181fc3395a3685c833a
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-18 21:21:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,22 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  require "yaml"
+  emoticons = YAML.load_file(path)
+  japenese_emoticons = emoticons.reduce({}) do |memo, (key, value)|
+    memo[value[1]] = value[0]
+  end 
+  p japenese_emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(traditional_emoticon)
+  
+  
 end
 
 def get_english_meaning
   # code goes here
 end
+
+load_library("./lib/emoticons.yml")
+
